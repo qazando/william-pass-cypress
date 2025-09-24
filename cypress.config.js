19c21dbb0b29896760f2a708ca982fa86c4f0a45
QAzando <65139046+qazando@users.noreply.github.com>
2025-09-24 19:56:57 -0300

    Subindo vídeo e screenshot

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "k93kiv",
   reporter: 'mochawesome',
+  video: true, // <-- ativa a gravação de vídeo
+  videosFolder: "cypress/videos", // pasta onde os vídeos ficam salvos
+  screenshotOnRunFailure: true, // tira screenshot quando o teste falha
   e2e: {
     baseUrl: "https://automationpratice.com.br",
     setupNodeEvents(on, config) {
